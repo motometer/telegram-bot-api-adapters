0875b0ad42ed58152b9687b89af502caab51dfbb
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-12-14 11:04:56 +0200

    Implement answer callback query. (#9)

--- src/main/java/org/motometer/telegram/bot/client/Methods.java
+++ src/main/java/org/motometer/telegram/bot/client/Methods.java
@@ -9,14 +9,18 @@ import java.util.List;
 
 final class Methods {
 
-    static final Method<User> ME = create("getMe", new TypeToken<ApiResponse<User>>() {
+    static final Method<User> ME = of("getMe", new TypeToken<ApiResponse<User>>() {
     });
-    static final Method<List<Update>> UPDATES = create("getUpdates", new TypeToken<ApiResponse<List<Update>>>() {
+    static final Method<List<Update>> UPDATES = of("getUpdates", new TypeToken<ApiResponse<List<Update>>>() {
     });
-    static final Method<Message> SEND_MESSAGE = create("sendMessage", new TypeToken<ApiResponse<Message>>() {
+    static final Method<Message> SEND_MESSAGE = of("sendMessage", new TypeToken<ApiResponse<Message>>() {
     });
+    static final Method<Boolean> ANSWER_CALLBACK_QUERY = of("answerCallbackQuery",
+        new TypeToken<ApiResponse<Boolean>>() {
+        }
+    );
 
-    private static <T> ImmutableMethod<T> create(String value, TypeToken<ApiResponse<T>> typeToken) {
+    private static <T> ImmutableMethod<T> of(String value, TypeToken<ApiResponse<T>> typeToken) {
         return ImmutableMethod.of(value, typeToken);
     }
 }
